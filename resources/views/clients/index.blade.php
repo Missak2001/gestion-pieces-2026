d73e0274104ca7604ae7049343cf7362b4dd3a16
missa <mmungan@esimed.fr>
2026-07-01 13:52:03 +0200

    Amélioration du design de l'interface

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -15,7 +15,7 @@
             @endif
 
             <a href="{{ route('clients.create') }}"
-               class="inline-flex items-center rounded-xl border border-cyan-800 bg-cyan-700 px-4 py-2 font-semibold text-white shadow-sm transition hover:-translate-y-0.5 hover:bg-cyan-800 hover:shadow">
+                    class="inline-flex items-center rounded-xl border border-cyan-800 bg-cyan-700 px-4 py-2 font-semibold text-white shadow-sm transition hover:-translate-y-0.5 hover:bg-cyan-800 hover:shadow">
                 Nouveau client
             </a>
 
